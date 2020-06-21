6eb1e8e78fca328b66057e8382aba80d0f7bec8f
Yeji Bae <61449209+yejiBae@users.noreply.github.com>
2020-06-22 07:57:22 +0900

    Delete information.py

--- src/information.py
+++
@@ -1,8 +0,0 @@
-
-def write_to_csv(file_name, arr):
-    file_to_write = open(file_name, 'a')
-    for item in arr:
-        file_to_write.write(str(item)+',')
-    file_to_write.write('\n')
-    file_to_write.close()
-    return 1
